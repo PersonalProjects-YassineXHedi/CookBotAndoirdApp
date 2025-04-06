5c9cd0293e0f2a44f7a9a6b54ba0ac1f23fb9fc9
Yassine SAHLI <ysahl091@uottawa.ca>
2025-04-05 22:24:00 -0400

    added manual ingredient adding

--- app/src/main/java/com/example/cookbot/IngredientTable.java
+++ app/src/main/java/com/example/cookbot/IngredientTable.java
@@ -1,5 +1,6 @@
 package com.example.cookbot;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -90,43 +91,66 @@ public class IngredientTable {
         addFinalRow();
     }
 
+    @SuppressLint("SetTextI18n")
     private void addFinalRow(){
         TableRow row = new TableRow(context);
 
-        TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
-        params.gravity = Gravity.CENTER_HORIZONTAL;
+        TableLayout.LayoutParams rowParams = new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT);
+        row.setLayoutParams(rowParams);
+        rowParams.setMargins(0,100,0,0);
 
-        Button addButton = new Button(context);
-        addButton.setText("Add Ingredient");
-        addButton.setGravity(Gravity.CENTER);
-        addButton.setBackgroundColor(Color.BLACK);
-        addButton.setTextColor(Color.WHITE);
-        addButton.setTextSize(15);
+        Button addIngredientButton = new Button(context);
+        addIngredientButton.setText("Add Ingredient");
+        addIngredientButton.setGravity(Gravity.CENTER);
+        addIngredientButton.setTextColor(Color.WHITE);
+        addIngredientButton.setBackgroundResource(R.drawable.rounded_button);
+        addIngredientButton.setTextSize(15);
 
-        addButton.setOnClickListener(v -> {
+        TableRow.LayoutParams buttonParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT);
+        buttonParams.span = 2;
+        buttonParams.gravity = Gravity.CENTER_HORIZONTAL;
+        addIngredientButton.setLayoutParams(buttonParams);
+
+        addIngredientButton.setOnClickListener(v -> {
             addInputRow();
         });
 
-        row.addView(addButton);
+        row.addView(addIngredientButton);
         tableLayout.addView(row);
     }
 
+    @SuppressLint("SetTextI18n")
     private void addInputRow(){
+        TableLayout.LayoutParams rowParams = new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT);
+        rowParams.setMargins(0,50,0,0);
+
         TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
         params.gravity = Gravity.CENTER_HORIZONTAL;
 
         TableRow row = new TableRow(context);
+        row.setLayoutParams(rowParams);
 
         EditText ingredientName = new EditText(context);
         ingredientName.setHint("Enter ingredient");
         ingredientName.setGravity(Gravity.CENTER);
         ingredientName.setTextSize(15);
-        ingredientName.setLayoutParams(params);
         ingredientName.setBackground(null);
         row.addView(ingredientName);
 
         Button addButton = new Button(context);
         addButton.setText("Add");
+        addButton.setTextColor(Color.WHITE);
+        addButton.setBackgroundColor(Color.BLACK);
+        addButton.setTextSize(13);
+        addButton.setGravity(Gravity.CENTER);
+        addButton.setLayoutParams(params);
+
         addButton.setOnClickListener(v -> {
             String name = ingredientName.getText().toString().trim();
             if (!name.isEmpty()) {
